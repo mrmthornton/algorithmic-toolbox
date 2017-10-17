70e36767d1e8321a20f53943f1dc36952c3c0ae1
mike <mike>
2017-10-17 15:04:16 -0400

    next homework

--- AplusB.py
+++ AplusB.py
@@ -1,11 +1,11 @@
-# Uses python3\n",
-# There are two ways of running this program:\n",
-# 1. Run\n",
-#     python3 APlusB.py\n",
-# then enter two numbers and press ctrl-d/ctrl-z\n",
-# 2. Save two numbers to a file -- say, dataset.txt.\n",
-# Then run\n",
-#     python3 APlusB.py < dataset.txt\n",
+# Uses python3
+# There are two ways of running this program:
+# 1. Run
+#     python3 APlusB.py
+# then enter two numbers and press ctrl-d/ctrl-z
+# 2. Save two numbers to a file -- say, dataset.txt.
+# Then run
+#     python3 APlusB.py < dataset.txt
 
 import sys
 
