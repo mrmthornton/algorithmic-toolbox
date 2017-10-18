c4cea16877afe0eb45aedf5958cb4ce2e344acbc
mrmthornton <mrmthornton@gmail.com>
2017-10-17 21:25:07 -0400

    finished week one

--- max_pairwise_product.py
+++ max_pairwise_product.py
@@ -1,7 +1,5 @@
 # Uses python3
-n = int(input())
-a = [int(x) for x in input().split()]
-assert(len(a) == n)
+
 
 #result = 0
 #
@@ -11,16 +9,36 @@ assert(len(a) == n)
 #        if temp > result:
 #            result = temp
 
-idx1 = 0
-for i in range(0, n):
-    if a[i] > a[idx1]:
-        idx1 = i
+def max_multi_fast(n,a):
 
-idx2 = 0
-for i in range(0, n):
-    if a[i] > a[idx2] and a[i] != a[idx1]:
-        idx2 = i
+    idx1 = 0 
+    for i in range(0, n):
+        if a[i] > a[idx1]:
+                idx1 = i
 
-        result =  a[idx1]*a[idx2]
+    idx2 = -1
+    for i in range(0, n):
+        # check that the current index is not equal to the first index,
+        # and that the current index has not been set or if set,
+        # the value of the current index is greater than the value of the prior index.
+        if i != idx1 and (idx2 == -1 or a[i] > a[idx2]): 
+            idx2 = i
 
-print(result)
+    result =  a[idx1]*a[idx2]
+    return(result)
+
+#how_many = int(input())
+#number_list = [int(x) for x in input().split()]
+#assert(len(number_list) == how_many)
+
+import numpy as np
+np.random.seed(1)
+#if __name__ == '__main__':
+while True:
+    how_many = np.random.randint(2,high=1000)
+    print(how_many)
+    number_list = [np.random.randint(1,high=100000) for i in range(how_many)]
+    print(len(number_list))
+
+    print(max_multi_fast(how_many,number_list))
+
